c82cfd78dd8b5256e3b1909c693e4cd570bdaa1b
Peter Quinn <peter.quinn396@gmail.com>
2018-07-29 18:18:34 -0400

    Implement Keras RL DQN agent into MoveToBeacon
    The keras RL DQN agent is now able to properly interact with the PySC2 on the MoveToBeacon map

--- rl_agent.py
+++ rl_agent.py
@@ -31,21 +31,21 @@ class MoveToBeacon_KerasRL(base_agent.BaseAgent):
     def __init__(self):
         super().__init__()
         self.cnt = 0
+
     def step(self, obs):
         # super().step(obs)
         if self.cnt == 0:
             self.cnt += 1
-            return (5,5)
+            return (5, 5)
         elif self.cnt == 1:
-            self.cnt+=1
-            return (5,50)
+            self.cnt += 1
+            return (5, 50)
         elif self.cnt == 2:
-            self.cnt+=1
-            return (50,50)
+            self.cnt += 1
+            return (50, 50)
         else:
             self.cnt = 0
-            return (50,5)
-
+            return (50, 5)
 
 
 class PySC2ToKerasRL_env(rl.core.Env):
@@ -56,10 +56,12 @@ class PySC2ToKerasRL_env(rl.core.Env):
 
         '''Move to Beacon'''
         # define the action space and obs space based on the map, this is the move to beacon
-        self.action_space = spaces.Box(np.array([0, 0]), np.array([63, 63]))  # select x,y points to move the marine to
-        # our minimap, 64x64 grid, with values 0-4 for who the units in the space belong to
-        self.observation_space = spaces.Box(np.zeros(64 ** 2), np.array([4 for x in range(0, 64 ** 2)])) # flatten minimap
+        self.action_space = spaces.Box(np.array(0), np.array(64**2))  #flatten action space
 
+        self.observation_space = spaces.Box(np.zeros(64 ** 2),
+                                            np.array([4 for x in range(0, 64 ** 2)]))  # flatten minimap
+
+        self.action_space.n = 64**2 # flatten the action space
 
     def step(self, step_actions):
         """Run one timestep of the environment's dynamics.
@@ -75,21 +77,24 @@ class PySC2ToKerasRL_env(rl.core.Env):
 
         # map x,y (0-63,0-63) coord to the move marine command
 
-        step_action = [actions.FUNCTIONS.Move_minimap("now", step_actions)]
+        x = step_actions % 64
+        y = step_actions // 64
+
+        step_action = [actions.FUNCTIONS.Move_minimap("now", (x,y))]
 
         try:
             timesteps = getattr(self.env, "step")(step_action)  # step env
         except:
-            timesteps = getattr(self.env, "step")([actions.FUNCTIONS.select_army('select')]) #select the marine
+            timesteps = getattr(self.env, "step")([actions.FUNCTIONS.select_army('select')])  # select the marine
 
         obs = timesteps[0].observation  # retrieve updated obs
 
-        obs = obs.feature_minimap.player_relative # a 0-63,0-63 array with 0-4 for each value
-        obs = obs.flatten() #map obs to a flattened space
+        obs = obs.feature_minimap.player_relative  # a 0-63,0-63 array with 0-4 for each value
+        obs = obs.flatten()  # map obs to a flattened space
 
         reward = timesteps[0].reward  # get reward
         done = timesteps[0].last()  # check if last step
-        info = None  # check if first step
+        info = {'items':1} # dummy dict entry to make keras rl happy
 
         return obs, reward, done, info
 
@@ -99,10 +104,10 @@ class PySC2ToKerasRL_env(rl.core.Env):
         # Returns
             observation (object): The initial observation of the space. Initial reward is assumed to be 0.
         """
-        getattr(self.env, "reset")() #reset env using parent method
+        getattr(self.env, "reset")()  # reset env using parent method
 
-        reset_action = [actions.FUNCTIONS.select_army('select')] #select marine
-        timesteps = getattr(self.env, "step")(reset_action) #select the marine
+        reset_action = [actions.FUNCTIONS.select_army('select')]  # select marine
+        timesteps = getattr(self.env, "step")(reset_action)  # select the marine
 
         obs = timesteps[0].observation  # retrieve initial obs
         # format obs
@@ -129,18 +134,45 @@ def main(unused_argv):
                                 step_mul=16,  # this gives roughly 150 apm (8 would give 300 apm)
                                 game_steps_per_episode=0,
                                 visualize=True) as env:
-
+                # create a keras-rl env
                 keras_env = PySC2ToKerasRL_env(env)
-                keras_agent = MoveToBeacon_KerasRL()
-                keras_agent.reset()
                 obs = keras_env.reset()
 
-                # check if first action, if so we automatically select the marine
+                # create an agent that can interact with
 
-                while True:
+                # Test Agent (makes marine run in circle)
+                # keras_agent = MoveToBeacon_KerasRL()
+                # keras_agent.reset()
+                # while True: #play the game
+                #
+                #     step_actions = keras_agent.step(obs)
+                #     obs, reward, done, info = keras_env.step(step_actions)
 
-                    step_actions = keras_agent.step(obs)
-                    obs, reward, done, info = keras_env.step(step_actions)
+                # Replace simple agent with a learning one
+                # A simple model (taken from Keras-RL cartpole dqn)
+                nb_actions = keras_env.action_space.n
+                model = Sequential()
+                model.add(Flatten(input_shape=(1,) + keras_env.observation_space.shape))
+                model.add(Dense(16))
+                model.add(Activation('relu'))
+                model.add(Dense(16))
+                model.add(Activation('relu'))
+                model.add(Dense(16))
+                model.add(Activation('relu'))
+                model.add(Dense(nb_actions))
+                model.add(Activation('linear'))
+                print(model.summary())
+
+                # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+                # even the metrics!
+                memory = SequentialMemory(limit=50000, window_length=1)
+                policy = BoltzmannQPolicy()
+                dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+                               target_model_update=1e-2, policy=policy)
+                dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+                # Okay, now it's time to learn something!
+                dqn.fit(keras_env, nb_steps=50000, visualize=False, verbose=2)
 
 
     except KeyboardInterrupt:
